9c39967c8b2f12cf147641cf45d3b3795d19e140
Ivan Gromakovskii <gie1994@gmail.com>
2017-03-15 20:46:04 +0300

    Extend if_guard_cmp example

--- examples/intraprocedural/control/if_guard_cmp.cpp
+++ examples/intraprocedural/control/if_guard_cmp.cpp
@@ -11,9 +11,19 @@ int main()
 {
     int * p = new int[8];
     int x = f();
-    if (x <= 3)
+    if (x <= 3) {
         if (x > 0)
             p[x] = 8;  // good
+
+        if (x - 2 > 0)
+            p[x] = 42;  // good
+
+    }
+
+    int y = -x;
+    if (x < 0)
+        if (x > -2)
+            p[y] = 7; // good
 
     p[x] = 9;  // bad
 
