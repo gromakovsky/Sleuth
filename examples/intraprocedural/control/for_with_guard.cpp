f85714d005622c66cc677bb672b74fa82fe10b70
Ivan Gromakovskii <gie1994@gmail.com>
2017-04-16 16:44:39 +0300

    Make for_with_guard example more interesting

--- examples/intraprocedural/control/for_with_guard.cpp
+++ examples/intraprocedural/control/for_with_guard.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-    int * arr = new int[10];
-    for (size_t x = 0; x < 10; ++x)
-        if (x > 0)
-            arr[x] = 5;
+    int * arr = new int[7];
+    for (int x = 0; x < 10; ++x)
+    {
+        if (x < 7)
+            arr[x] = 5;  // ok
+
+        arr[x] = 6;  // bad
+    }
 
     return 0;
 }
