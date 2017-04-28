8fff5e475e17e9790833681516d5e1ec91ced578
Ivan Gromakovskii <gie1994@gmail.com>
2017-04-28 20:02:59 +0300

    Add arg_ranges to context

--- src/context.h
+++ src/context.h
@@ -13,7 +13,8 @@
 
 using new_val_set_t = std::unordered_set<var_id>;
 
-struct vulnerability_info_t {
+struct vulnerability_info_t
+{
     // true = vulnerable
     // false = not vulnerable
     // interminated = may be vulnerable, but we don't know for sure
@@ -22,6 +23,31 @@ struct vulnerability_info_t {
     sym_range size_range;
 };
 
+// information about function's argument
+struct argument_t
+{
+    llvm::Function const * func;
+    size_t idx;
+
+    bool operator==(argument_t const & other) const
+    {
+        return func == other.func && idx == other.idx;
+    }
+};
+
+namespace std {
+
+  template <>
+  struct hash<argument_t>
+  {
+    std::size_t operator()(argument_t const & arg) const
+    {
+      return hash<llvm::Function const *>()(arg.func) ^ hash<size_t>()(arg.idx);
+    }
+  };
+
+}
+
 struct context_t
 {
     std::unordered_map<var_id, sym_range> def_ranges;
@@ -29,4 +55,5 @@ struct context_t
     std::unordered_map<var_id, vulnerability_info_t> vulnerability_info;
     llvm::TargetLibraryInfoWrapperPass tliwp;
     llvm::DominatorTreeWrapperPass dtwp;
+    std::unordered_map<argument_t, sym_range> arg_ranges;
 };
